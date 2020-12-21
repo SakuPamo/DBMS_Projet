e689056ff386c6c05de5e929bd8f57af1252cbab
Tharuke Jayawardhane <tharuerox@hotmail.com>
2020-12-21 07:59:00 +0530

    adding database and add the config file

---
+++ Utility/Globle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBMSProjet.Utility
+{
+    public class Globle
+    {
+        public static bool IsCutomer { get; set; }
+        public static bool IsLog { get; set; }
+
+        //public static UserManegement userManegement { get; set; }
+    }
+}
